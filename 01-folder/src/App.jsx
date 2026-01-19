7eeae8bfe83de8267c5415432e8841e8a9813d92
aamiransari1567 <aamiransari1567@gmail.com>
2026-01-19 18:20:44 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1>Aamir</h1>
+    </div>
+    <div>
+      <h2>Ansari</h2>
+    </div>
+    <div>
+      <h3>Hero</h3>
+    </div>
+    </>
+  )
+}
+
+export default App
+
